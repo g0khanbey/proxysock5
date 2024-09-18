d24eda661952d63aa6556458b30f40351f29b447
gokhanbey <97981071+g0khanbey@users.noreply.github.com>
2024-09-18 15:50:45 +0300

    Update socks5.sh

--- socks5.sh
+++ socks5.sh
@@ -1,29 +1,20 @@
 #!/bin/bash
-
-# Swap alanı oluşturma
-echo "4 GB Swap alanı oluşturuluyor..."
-sudo fallocate -l 4G /swapfile
+sudo fallocate -l 8G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
-echo '/swapfile swap swap defaults 0 0' | sudo tee -a /etc/fstab
-
-# Swap ayarları tamamlandı
-echo "Swap alanı başarıyla oluşturuldu ve etkinleştirildi."
-
-# Proxy için kullanıcı bilgilerini alma
-echo -e "Lütfen socks5 proxy için bir kullanıcı adı girin:"
+echo -e "Please enter the username for the socks5 proxy:"
 read username
-echo -e "Lütfen socks5 proxy için bir şifre girin:"
+echo -e "Please enter the password for the socks5 proxy:"
 read -s password
 
-# Repositories güncelleme
+# Update repositories
 sudo apt update -y
 
-# Dante-server kurulumu
+# Install dante-server
 sudo apt install dante-server -y
 
-# Konfigürasyon dosyasını oluşturma
+# Create the configuration file
 sudo bash -c 'cat <<EOF > /etc/danted.conf
 logoutput: /var/log/danted.log
 internal: 0.0.0.0 port = 1080
@@ -41,33 +32,26 @@ socks pass {
 }
 EOF'
 
-# Proxy için kullanıcı ve şifre oluşturma
+# Add user with password
 sudo useradd --shell /usr/sbin/nologin $username
 echo "$username:$password" | sudo chpasswd
 
-# UFW kontrolü ve port açma
+# Check if UFW is active and open port 1080 if needed
 if sudo ufw status | grep -q "Status: active"; then
     sudo ufw allow 1080/tcp
-    echo "UFW aktif, 1080 portu açıldı."
-else
-    echo "UFW devre dışı, port açılmadı."
 fi
 
-# iptables kontrolü ve port açma
+# Check if iptables is active and open port 1080 if needed
 if sudo iptables -L | grep -q "ACCEPT     tcp  --  anywhere             anywhere             tcp dpt:1080"; then
-    echo "Port 1080 iptables'ta zaten açık."
+    echo "Port 1080 is already open in iptables."
 else
     sudo iptables -A INPUT -p tcp --dport 1080 -j ACCEPT
-    echo "iptables'ta 1080 portu açıldı."
 fi
 
-# Dante-server'ı yeniden başlatma
+# Restart dante-server
 sudo systemctl restart danted
 
-# Dante-server'ın başlatılmasını etkinleştirme
+# Enable dante-server to start at boot
 sudo systemctl enable danted
 
-# systemd-oomd'yi kaldırma
-sudo apt remove systemd-oomd -y
-
-echo "Dante SOCKS5 Proxy kurulumu tamamlandı ve swap alanı başarıyla eklendi."
+sudo apt remove systemd-oomd
